3361f0a6c4bb05c170547466f41ffccb13906e0f
Hamerzin <107520793+Hamerzin@users.noreply.github.com>
2022-09-10 14:03:38 -0300

    Update settings.py

--- nuevoblog/settings.py
+++ nuevoblog/settings.py
@@ -162,8 +162,12 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
+EMAIL_HOST = ''
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = ''
+DEFAULT_FROM_EMAIL = ''
 
 
 
